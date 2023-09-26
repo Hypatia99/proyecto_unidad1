513d0fba164f130e794952c53ee1d1ca1c25f67e
Grecia Hypatiagizeh Duran Navarro <hypadn@gmail.com>
2023-09-26 11:50:16 -0600

    clase_25y 26

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:proyecto_unidad1/screens/screens.dart';
 
 void main() => runApp(const MyApp());
+
+//Modo perezoso, no se crea hasta que alguien lo cree
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => MoviesProvider(),
+          //Que se cree en cuanto se cree la aplicacion
+          lazy: false,
+        )
+      ],
+      child: MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
